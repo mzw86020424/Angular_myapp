34ffd95e57710ad54651d4228102076406f43883
YutaMizusawa <yuta.mizusawa@dive.design>
2021-04-15 12:11:51 +0900

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { UserListComponent } from "./user/user-list/user-list.component";
+import { UserEditComponent } from "./user/user-edit/user-edit.component";
+
+const routes: Routes = [
+  // http://～/ → Dashboard にリダイレクト
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // http://～/dashbord → Dashboard を表示
+  { path: 'dashboard', component: DashboardComponent },
+  // http://～/users → ユーザ一覧を表示
+  { path: 'users', component: UserListComponent },
+  // http://～/users/{user_id}/edit → ユーザ更新画面を表示
+  { path: 'users/:id/edit', component: UserEditComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
